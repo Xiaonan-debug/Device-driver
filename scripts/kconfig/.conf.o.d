8d5e968a5c386939dd9d69dbaa11331a344e4166
Xiaonan LUO <xluobd@Xiaonans-MacBook-Pro.local>
2023-03-20 11:14:48 +0800

    First commit

---
+++ scripts/kconfig/.conf.o.d
@@ -0,0 +1,2 @@
+compiler-file13766022944818688927.o: \
+ /tmp/compiler-file13766022944818688927 /usr/include/stdc-predef.h
---
+++ src/acpi/acpica/.utmisc.o.d
@@ -0,0 +1,2 @@
+compiler-file13766022944818688927.o: \
+ /tmp/compiler-file13766022944818688927 include/autoconf.h
